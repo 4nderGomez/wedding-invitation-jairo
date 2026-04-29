7c11fc2db9f16380bb2deebb8fed35e431fb2fbe
Ander Gomez <alexander.david.g.j@gmail.com>
2026-04-29 00:02:01 -0600

    Inicializar setup de proyecto-2

---
+++ src/main/java/com/wedding/invitationjairo/InvitationjairoApplication.java
@@ -0,0 +1,13 @@
+package com.wedding.invitationjairo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvitationjairoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvitationjairoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wedding/invitationjairo/InvitationjairoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedding.invitationjairo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvitationjairoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

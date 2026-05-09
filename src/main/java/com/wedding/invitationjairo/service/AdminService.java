ff95ca34675626117e77beb79c9fe79c75ab7af8
Ander Gomez <alexander.david.g.j@gmail.com>
2026-05-08 23:55:13 -0600

    Correción de AdminService

--- src/main/java/com/wedding/invitationjairo/service/AdminService.java
+++ src/main/java/com/wedding/invitationjairo/service/AdminService.java
@@ -13,7 +13,6 @@ import com.wedding.invitationjairo.repository.GuestCompanionRepository;
 import com.wedding.invitationjairo.repository.GuestGroupRepository;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
